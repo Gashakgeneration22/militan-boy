54c7f7f01e4928a095405dc1411abc9db9a2f5b5
Gashakgeneration22 <97940348+Gashakgeneration22@users.noreply.github.com>
2022-01-18 18:11:09 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
-IZIN=$( curl https://raw.githubusercontent.com/dpvpn09/gaspoll/main/ipvps | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/Gashakgeneration22/militan-boy/main/ipvps | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${green}Permission Accepted...${NC}"
 else
