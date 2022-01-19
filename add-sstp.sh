447e41399cd85013457d7e7e0b5079ac14db5482
Gashakgeneration22 <97940348+Gashakgeneration22@users.noreply.github.com>
2022-01-19 12:48:49 +0700

    Update add-sstp.sh

--- add-sstp.sh
+++ add-sstp.sh
@@ -3,7 +3,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ipv4.wildyproject.com);
-echo "Script By YakkoXCode"
+echo "Script By Jojo22XCode"
 clear
 IP=$(wget -qO- icanhazip.com);
 sstp="$(cat ~/log-install.txt | grep -i SSTP | cut -d: -f2|sed 's/ //g')"
