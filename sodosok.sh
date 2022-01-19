5fa701822a437337359bf3c92cfd4baa381b3519
Gashakgeneration22 <97940348+Gashakgeneration22@users.noreply.github.com>
2022-01-19 10:40:34 +0700

    Update sodosok.sh

--- sodosok.sh
+++ sodosok.sh
@@ -88,10 +88,10 @@ iptables -I INPUT -m state --state NEW -m udp -p udp --dport 2443:3543 -j ACCEPT
 iptables-save > /etc/iptables.up.rules
 ip6tables-save > /etc/ip6tables.up.rules
 cd /usr/bin
-wget -O add-ss "https://raw.githubusercontent.com/YakkoRasZ14/gaspoll/main/add-ss.sh"
-wget -O del-ss "https://raw.githubusercontent.com/YakkoRasZ14/gaspoll/main/del-ss.sh"
-wget -O cek-ss "https://raw.githubusercontent.com/YakkoRasZ14/gaspoll/main/cek-ss.sh"
-wget -O renew-ss "https://raw.githubusercontent.com/YakkoRasZ14/gaspoll/main/renew-ss.sh"
+wget -O add-ss "https://raw.githubusercontent.com/Gashakgeneration22/militan-boy/main/add-ss.sh"
+wget -O del-ss "https://raw.githubusercontent.com/Gashakgeneration22/militan-boy/main/del-ss.sh"
+wget -O cek-ss "https://raw.githubusercontent.com/Gashakgeneration22/militan-boy/main/cek-ss.sh"
+wget -O renew-ss "https://raw.githubusercontent.com/Gashakgeneration22/militan-boy/main/renew-ss.sh"
 chmod +x add-ss
 chmod +x del-ss
 chmod +x cek-ss
