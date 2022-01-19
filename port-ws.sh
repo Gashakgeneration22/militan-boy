5bd9595e6e33c49cf3b852e1c2639570999a94fe
Gashakgeneration22 <97940348+Gashakgeneration22@users.noreply.github.com>
2022-01-19 21:17:44 +0700

    Update port-ws.sh

--- port-ws.sh
+++ port-ws.sh
@@ -3,7 +3,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ipv4.wildyproject.com);
-echo "Script By YakkoXCode"
+echo "Script By Jojo22XCode"
 clear
 tls="$(cat ~/log-install.txt | grep -w "Vmess TLS" | cut -d: -f2|sed 's/ //g')"
 none="$(cat ~/log-install.txt | grep -w "Vmess None TLS" | cut -d: -f2|sed 's/ //g')"
