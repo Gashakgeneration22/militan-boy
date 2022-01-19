e4beb439486c498b60ac140cf601c750808d488e
Gashakgeneration22 <97940348+Gashakgeneration22@users.noreply.github.com>
2022-01-19 21:14:52 +0700

    Update port-tr.sh

--- port-tr.sh
+++ port-tr.sh
@@ -3,7 +3,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ipv4.wildyproject.com);
-echo "Script By YakkoXCode"
+echo "Script By Jojo22XCode"
 clear
 tr="$(cat ~/log-install.txt | grep -i Trojan | cut -d: -f2|sed 's/ //g')"
 echo -e "      Change Port $tr"
