c4d823bd5b75b6b8ace56808f0ec1c0ed3bdbf20
Gashakgeneration22 <97940348+Gashakgeneration22@users.noreply.github.com>
2022-01-19 20:47:32 +0700

    Update cek-sstp.sh

--- cek-sstp.sh
+++ cek-sstp.sh
@@ -3,7 +3,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ipv4.wildyproject.com);
-echo "Script By YakkoXCode"
+echo "Script By Jojo22XCode"
 clear
 accel-cmd show sessions
 echo ""
