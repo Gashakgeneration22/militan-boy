b162c2967bd857c3efa7b3626784749aa213f2e0
Gashakgeneration22 <97940348+Gashakgeneration22@users.noreply.github.com>
2022-01-19 10:35:44 +0700

    Update set-br.sh

--- set-br.sh
+++ set-br.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl https://rclone.org/install.sh | bash
 printf "q\n" | rclone config
-wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/YakkoRasZ14/gaspoll/main/rclone.conf"
+wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/Gashakgeneration22/militan-boy/main/rclone.conf"
 git clone  https://github.com/magnific0/wondershaper.git
 cd wondershaper
 make install
@@ -26,12 +26,12 @@ logfile ~/.msmtp.log
 EOF
 chown -R www-data:www-data /etc/msmtprc
 cd /usr/bin
-wget -O autobackup "https://raw.githubusercontent.com/YakkoRasZ14/gaspoll/main/autobackup.sh"
-wget -O backup "https://raw.githubusercontent.com/YakkoRasZ14/gaspoll/main/backup.sh"
-wget -O bckp "https://raw.githubusercontent.com/YakkoRasZ14/gaspoll/main/bckp.sh"
-wget -O restore "https://raw.githubusercontent.com/YakkoRasZ14/gaspoll/main/restore.sh"
-wget -O strt "https://raw.githubusercontent.com/YakkoRasZ14/gaspoll/main/strt.sh"
-wget -O limit-speed "https://raw.githubusercontent.com/YakkoRasZ14/gaspoll/main/limit-speed.sh"
+wget -O autobackup "https://raw.githubusercontent.com/Gashakgeneration22/militan-boy/main/autobackup.sh"
+wget -O backup "https://raw.githubusercontent.com/Gashakgeneration22/militan-boy/main/backup.sh"
+wget -O bckp "https://raw.githubusercontent.com/Gashakgeneration22/militan-boy/main/bckp.sh"
+wget -O restore "https://raw.githubusercontent.com/Gashakgeneration22/militan-boy/main/restore.sh"
+wget -O strt "https://raw.githubusercontent.com/Gashakgeneration22/militan-boy/main/strt.sh"
+wget -O limit-speed "https://raw.githubusercontent.com/Gashakgeneration22/militan-boy/main/limit-speed.sh"
 chmod +x autobackup
 chmod +x backup
 chmod +x bckp
