c5e9a7a52f33c9ed28db59f92fba960cb027f2a1
Gashakgeneration22 <97940348+Gashakgeneration22@users.noreply.github.com>
2022-01-19 21:38:20 +0700

    Update trojaan.sh

--- trojaan.sh
+++ trojaan.sh
@@ -6,7 +6,7 @@ bl='\e[36;1m'
 bd='\e[1m'
 MYIP=$(wget -qO- ifconfig.co);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/YakkoRasZ14/gaspoll/main/ipvps | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/Gashakgeneration22/militan-boy/main/ipvps | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${green}Permission Accepted...${NC}"
 else
