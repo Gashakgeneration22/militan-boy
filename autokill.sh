3a26a1f3183720186f479c2e4babdcd9062be5b3
Gashakgeneration22 <97940348+Gashakgeneration22@users.noreply.github.com>
2022-01-19 10:45:30 +0700

    Update autokill.sh

--- autokill.sh
+++ autokill.sh
@@ -3,7 +3,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ipv4.wildyproject.com);
-echo "Script By YakkoXCode"
+echo "Script By Jojo22XCode"
 clear
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[ON]${Font_color_suffix}"
