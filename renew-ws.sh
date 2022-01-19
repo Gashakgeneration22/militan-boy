2c26b40d17621f2081002dfd84a809275e35bdc6
Gashakgeneration22 <97940348+Gashakgeneration22@users.noreply.github.com>
2022-01-19 10:26:10 +0700

    Update renew-ws.sh

--- renew-ws.sh
+++ renew-ws.sh
@@ -3,7 +3,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ipv4.wildyproject.com);
-echo "Script By YakkoXCode"
+echo "Script By Jojo22XCode"
 clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/v2ray/config.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
